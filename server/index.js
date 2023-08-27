4ffaa845776e910d1a666272b920e3e32662855d
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2023-08-27 20:38:51 +0530

    feat: Dockerfile

--- server/index.js
+++ server/index.js
@@ -28,6 +28,8 @@ app.get("/",(req,res)=>{
 app.use('/auth', authRoutes);
 app.use('/webhook', webhookRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 });
